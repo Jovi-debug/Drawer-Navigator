1b0a05f532683e1df497b18bd27a8e8904b5392a
Bonjov Hima <bonjovhima@hotmail.de>
2025-05-21 22:12:27 +0200

    test configuring drawer (sidebar)

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "react-native-vector-icons";
+
+const DrawerSidebar = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <DrawerSidebar.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#20b2aa" },
+          headerTintColor: "white",
+          drawerActiveBackgroundColor: "#ffdead",
+          drawerActiveTintColor: "#ff4500",
+          drawerStyle: { backgroundColor: "#fffafa" },
+        }}
+      >
+        <DrawerSidebar.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+        <DrawerSidebar.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </DrawerSidebar.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
